f42455655287f653d9e899f8b7716359ca5dd04f
Edward Anthony Escudero Bowie <57167218+Kayeddy@users.noreply.github.com>
2022-06-22 16:27:10 -0500

    Changed some of the files extensions

--- kylog/pages/index.jsx
+++ kylog/pages/index.jsx
@@ -1,20 +1,9 @@
-import type { NextPage } from 'next'
+import { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { PostCard, Categories, PostWidget } from '../components'
 
-const posts = [
-  {
-    title: 'React testing',
-    excerpt: 'Learn React testing'
-  },
-
-  {
-    title: 'React with Tailwind',
-    excerpt: 'Learn React with Tailwind'
-  }
-];
-
-const Home: NextPage = () => {
+const Home = () => {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -23,24 +12,13 @@ const Home: NextPage = () => {
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
-          {
-            posts.map((post, index) => (
-              <div key={`post-${index}`}>
-                {
-                  post.title
-                }
-                {
-                  post.excerpt
-                }
-              </div>
-            ))
-          }
-
+          <PostCard />
         </div>
 
         <div className='lg:col-span-4 col-span-1'>
           <div className='lg:sticky relative top-8'>
-            
+            <PostWidget />
+            <Categories />
           </div>
         </div>
 

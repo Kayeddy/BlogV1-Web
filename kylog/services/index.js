a4d1f57da92bad02f0d194d3d5d715fe9fd21323
Edward Anthony Escudero Bowie <57167218+Kayeddy@users.noreply.github.com>
2022-06-27 03:31:44 -0500

    Update index.js

--- kylog/services/index.js
+++ kylog/services/index.js
@@ -1,1 +1,39 @@
 import {request, gql } from 'graphql-request' 
+const graphqlApi= process.env.PUBLIC_GRAPHCMS_ENDPOINT;
+
+export const getPost = async() => {
+
+    const query= gql`
+        query myQuery {
+            postsConnection {
+                edges {
+                  node {
+                    authors {
+                      bio
+                      id
+                      name
+                      photo {
+                        url
+                      }
+                      post {
+                        categories {
+                          name
+                          slug
+                        }
+                      }
+                    }
+                    createdAt
+                    slug
+                    title
+                    excerpt
+                    featuredImage {
+                      url
+                    }
+                  }
+                }
+            }
+        }
+    `
+
+    const result = await  request(graphQlApi, query);
+}

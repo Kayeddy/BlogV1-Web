5efc6cd21cd7193894c96fb8ef5464db76ad5cf5
Edward Anthony Escudero Bowie <57167218+Kayeddy@users.noreply.github.com>
2022-11-23 14:06:03 -0500

    Updated Comments

--- kylog/components/Comments.jsx
+++ kylog/components/Comments.jsx
@@ -1,56 +1,50 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import moment from 'moment'
-import parse from 'html-react-parser'
-import { getComments } from '../services'
+import React from "react";
+import { useState, useEffect } from "react";
+import moment from "moment";
+import parse from "html-react-parser";
+import { getComments } from "../services";
+import { useRouter } from "next/router";
 
-const Comments = ( { slug }) => {
+const Comments = ({ slug }) => {
+  const [comments, setComments] = useState([]);
 
-  const [comments, setComments] = useState([])
+  const router = useRouter();
 
+  //function that fetches post comments when the slug changes
   useEffect(() => {
-    getComments(slug).then(res => setComments(res))
-  }, [])
-  
+    getComments(slug).then((res) => {
+      setComments(res);
+    });
+    console.log("UEA");
+  }, [router.pathname]);
+
   return (
     <h1>
-        <>
-          {
-            comments.length > 0 && (
-              <div className='bg-white shadow-lg rounded-lg p-8 pb-12 mb-6'>
-                <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
-                  { comments.length }
-                  {' '}
-                  { comments.length > 1 ? 'comments' : 'comment'}
-                </h3>
-                {
-                  comments.map((comment) => (
-                    <div key={ comment.createdAt} className='border-b border-gray-100 mb-4 pb-4'>
-                      <p className='mb-4'>
-                        <span className='font-semibold'>
-                          { comment.name }
-                        </span>
-                        {' '}
-                        on
-                        {' '}
-                        {
-                          moment(comment.createdAt).format('MMM, DD, YYYY')
-                        }
-                      </p>
-                      <p className='whitespace-pre-line text-gray-600 w-full'>
-                        {
-                          parse(comment.comment)
-                        }
-                      </p>
-                    </div>
-                  ))
-                }
+      <>
+        {comments.length > 0 && (
+          <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-6">
+            <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+              {comments.length} {comments.length > 1 ? "comments" : "comment"}
+            </h3>
+            {comments.map((comment) => (
+              <div
+                key={comment.createdAt}
+                className="border-b border-gray-100 mb-4 pb-4"
+              >
+                <p className="mb-4">
+                  <span className="font-semibold">{comment.name}</span> on{" "}
+                  {moment(comment.createdAt).format("MMM, DD, YYYY")}
+                </p>
+                <p className="whitespace-pre-line text-gray-600 w-full">
+                  {parse(comment.comment)}
+                </p>
               </div>
-            )
-          }
-        </>
+            ))}
+          </div>
+        )}
+      </>
     </h1>
-  )
-}
+  );
+};
 
-export default Comments
+export default Comments;
--- kylog/components/CommentsForm.jsx
+++ kylog/components/CommentsForm.jsx
@@ -1,8 +1,7 @@
-import React, { useState, useEffect, useRef } from 'react'
-import { submitComment } from '../services';
+import React, { useState, useEffect, useRef } from "react";
+import { submitComment } from "../services";
 
 const CommentsForm = ({ slug }) => {
-
   const [error, setError] = useState(false);
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
 
@@ -12,76 +11,115 @@ const CommentsForm = ({ slug }) => {
   const storeDataLocally = useRef();
 
   useEffect(() => {
-    commentatorName.current.value = window.localStorage.getItem('name');
-    commentatorEmail.current.value = window.localStorage.getItem('email');
-  }, [])
-  
+    commentatorName.current.value = window.localStorage.getItem("name");
+    commentatorEmail.current.value = window.localStorage.getItem("email");
+  }, []);
 
   const handleCommentSubmission = () => {
-    setError(false)
+    setError(false);
 
     const { value: comment } = commentElement.current;
     const { value: name } = commentatorName.current;
     const { value: email } = commentatorEmail.current;
     const { checked: storeData } = storeDataLocally.current;
 
-    if (!comment || !name || !email ) {
-        setError(true);
-        return; // This return statement allows us to step out (stop) of the function (the comment submission) once our conditional finishes it's duty
+    if (!comment || !name || !email) {
+      setError(true);
+      return; // This return statement allows us to step out (stop) of the function (the comment submission) once our conditional finishes it's duty
     }
 
     const commentObject = {
-      name, email, comment, slug
-    }
+      name,
+      email,
+      comment,
+      slug,
+    };
 
-    if(storeData) {
-      window.localStorage.setItem('name', name);
-      window.localStorage.setItem('email', email);
+    if (storeData) {
+      window.localStorage.setItem("name", name);
+      window.localStorage.setItem("email", email);
     } else {
-      window.localStorage.removeItem('name', name);
-      window.localStorage.removeItem('email', email);
+      window.localStorage.removeItem("name", name);
+      window.localStorage.removeItem("email", email);
     }
 
     submitComment(commentObject).then((res) => {
-      setShowSuccessMessage(true)
+      setShowSuccessMessage(true);
       setTimeout(() => {
-        setShowSuccessMessage(false)
-      }, 3000)
-    })
-  }
+        setShowSuccessMessage(false);
+      }, 3000);
+    });
+  };
 
   return (
-    <div className='bg-white shadow-lg rounded-lg p-8 pb-12 mb-8'>
-        <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
-          Comment area
-        </h3>
+    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">Comment area</h3>
 
-        <div className='grid grid-cols-1 gap-4 mb-4'>
-          <textarea name= "Comment" id= "" ref= { commentElement } placeholder = 'Leave a comment here' className= 'p-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-100 bg-gray-200 text-gray-700' />
+      <div className="grid grid-cols-1 gap-4 mb-4">
+        <textarea
+          name="Comment"
+          id=""
+          ref={commentElement}
+          placeholder="Leave a comment here"
+          className="p-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-100 bg-gray-200 text-gray-700"
+        />
+      </div>
+
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4">
+        <input
+          type="text"
+          ref={commentatorName}
+          placeholder="Your name"
+          className="p-2 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-100 bg-gray-200 text-gray-700"
+        />
+        <input
+          type="text"
+          ref={commentatorEmail}
+          placeholder="Your email address"
+          className="p-2 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-100 bg-gray-200 text-gray-700"
+        />
+      </div>
+
+      <div className="grid grid-cols-1 gap-4 my-5">
+        <div className="flex align-center gap-2">
+          <input
+            type="checkbox"
+            ref={storeDataLocally}
+            id="storeData"
+            name="storeData"
+            value={true}
+          />
+          <label htmlFor="storeData" className="text-gray-500 cursor-pointer">
+            {" "}
+            Save email and name for future comments{" "}
+          </label>
         </div>
+      </div>
 
-        <div className='grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4'>
-          <input type="text" ref={ commentatorName } placeholder = 'Your name' className = 'p-2 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-100 bg-gray-200 text-gray-700' />
-          <input type="text" ref={ commentatorEmail } placeholder = 'Your email address' className = 'p-2 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-100 bg-gray-200 text-gray-700' />
-        </div>
+      {error && (
+        <p className="text-xs text-red-500">
+          {" "}
+          In order to leave a comment all fields must be filled out.{" "}
+        </p>
+      )}
 
-        <div className='grid grid-cols-1 gap-4 my-5'> 
-          <div className='flex align-center gap-2'>
-            <input type= "checkbox" ref= { storeDataLocally } id= 'storeData' name='storeData' value={ true }/>
-            <label htmlFor= 'storeData' className='text-gray-500 cursor-pointer'> Save email and name for future comments </label>
-          </div>
-        </div>
-
-        { error && <p className='text-xs text-red-500'> In order to leave a comment all fields must be filled out. </p> }
-
-        <div className='mt-8 text-center'>
-          <button type = 'button' onClick={ handleCommentSubmission } className='transition duration-500 ease-in-out hover:bg-indigo-900 inline-block text-white bg-pink-600 text-lg rounded-full p-3 cursor-pointer'>
-            Post comment
-          </button>
-          { showSuccessMessage && <p className='text-xl font-semibold mt-3 text-green-500'> Your comment has been submitted for review! </p>}
-        </div>
+      <div className="mt-8 text-center">
+        <button
+          type="button"
+          onClick={handleCommentSubmission}
+          className="transition duration-500 ease-in-out hover:bg-indigo-900 inline-block text-white bg-pink-600 text-lg rounded-full p-3 cursor-pointer"
+        >
+          Post comment
+        </button>
+        {showSuccessMessage && (
+          <p className="text-xl font-semibold mt-3 text-green-500">
+            {" "}
+            Your comment has been submitted for review!{" "}
+          </p>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CommentsForm
+export default CommentsForm;
